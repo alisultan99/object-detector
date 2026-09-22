e0e63b3186030543943831a5efcc7c9a6392a94d
alisultan99 <alisultanytr4@gmail.com>
2026-09-22 11:08:57 +0500

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Live Object Detector',
+  description: 'AI Video Object Detection App',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
